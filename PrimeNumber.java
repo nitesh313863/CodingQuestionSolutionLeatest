325b212a7d2139bd13765fb8172e24e0f2f12055
Nitesh Yadav <ny313863@gmail.com>
2022-08-06 20:27:17 +0530

    input i=5 || Prime Numer

---
+++ PrimeNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n=sc.nextInt();
+        boolean flag=false;
+        if(n==0 || n==1)
+            {
+                flag=false;
+            }
+        for(int i=2;i<=n/2;i++)
+        {
+            
+           if(n%i==0)
+            {
+                flag=true;
+                break;
+            }
+            
+        }
+        if(flag==true)
+            {
+                System.out.println(" This Number Not Prime Number");
+            }
+            else
+            {
+                System.out.println("This Number Is Prime Number");
+            }
+
+    }
+    
+}
