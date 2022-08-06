50453db77168a84e38fa8d3a36e10d6b89f2d89a
Nitesh Yadav <ny313863@gmail.com>
2022-08-06 11:33:48 +0530

    input arr[1,2,3,4,5] || input arr2[7,8,1,2,3] || O/P = 1 2 3

---
+++ FibonacciSeries.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the N Number");
+        int n=sc.nextInt();
+        int a=0;
+        int b=1;
+        int c;
+        for(int i=0;i<=n;i++)
+        {
+            if(i==a || i==b)
+            {
+                System.out.print("  "+i);
+            }
+            else
+            {
+                c=a+b;
+                System.out.print(" "+c);
+                a=b;
+                b=c;
+            }
+        }
+
+    }
+    
+}
--- IntersectionTwoArray.java
+++ IntersectionTwoArray.java
@@ -1,3 +1,10 @@
 public class IntersectionTwoArray {
-    
+    public static void main(String[] args) {
+        int arr[]={1,2,6,4,4};
+    int arr1[]={7,8,9,2,4,6,1};
+    for(int i=0;i<=arr.length-1;i++)
+        for (int j = 0; j <= arr1.length - 1; j++)
+            if (arr[i] == arr1[j])
+                System.out.print(" "+arr[i]);
+    }
 }
